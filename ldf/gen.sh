c2b31e327053c7ab52a70e26cebfe8d086244d9d
phucnv282 <phucnv282@gmail.com>
2021-01-12 22:40:39 +0700

    sh

--- ldf/gen.sh
+++ ldf/gen.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-./ldf -i ../data/facebook.adj -o ../data/facebookComm.txt
-./ldf -i ../data/wiki.adj -o ../data/wikiComm.txt
-./ldf -i ../data/epinions.adj -o ../data/epinionsComm.txt
-./ldf -i ../data/dblp.adj -o ../data/dblpComm.txt
+#./ldf -i ../data/facebook.adj -o ../data/facebookComm.txt
+#./ldf -i ../data/wiki.adj -o ../data/wikiComm.txt
+#./ldf -i ../data/epinions.adj -o ../data/epinionsComm.txt
+#./ldf -i ../data/dblp.adj -o ../data/dblpComm.txt
+./ldf -i ../data/pokec.adj -o ../data/pokecComm.txt
 
 rm -rf tmp1.adj
