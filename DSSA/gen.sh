43282747772e3515d77825ab1cbba7cf32ef1458
phucnv282 <phucnv282@gmail.com>
2021-01-14 21:46:36 +0700

    add DSSA

--- DSSA/gen.sh
+++ DSSA/gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#./el2bin ../data/facebookSSA.txt ../data/facebookSSA.bin
-#./el2bin ../data/wikiSSA.txt ../data/wikiSSA.bin
-#./el2bin ../data/epinionsSSA.txt ../data/epinionsSSA.bin
-#./el2bin ../data/dblpSSA.txt ../data/dblpSSA.bin
+./el2bin ../data/facebookSSA.txt ../data/facebookSSA.bin
+./el2bin ../data/wikiSSA.txt ../data/wikiSSA.bin
+./el2bin ../data/epinionsSSA.txt ../data/epinionsSSA.bin
+./el2bin ../data/dblpSSA.txt ../data/dblpSSA.bin
 ./el2bin ../data/pokecSSA.txt ../data/pokecSSA.bin
