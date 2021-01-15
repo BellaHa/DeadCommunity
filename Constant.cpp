e5026640e1433e52627e74e29ae3a7f089d5e81b
phucnv282 <phucnv282@gmail.com>
2021-01-15 14:05:52 +0700

    Constant

--- Constant.cpp
+++ Constant.cpp
@@ -1,20 +1,17 @@
 #include "Constant.h"
 
-
-
-Constant::Constant()
-{
+Constant::Constant() {
 }
 
-
-Constant::~Constant()
-{
+Constant::~Constant() {
 }
 
+int Constant::K = 10;
+bool Constant::IS_WEIGHTED = false;
+int Constant::COMMUNITY_POPULATION = 8;
 const double Constant::PERCENTAGE_THRESHOLD = 0.5;
 const double Constant::EPSILON = 0.2;
 const double Constant::DELTA = 0.2;
-int Constant::K = 10;
+const int Constant::NUM_THREAD = 4;
 bool Constant::IS_BOUNDED_THRESHOLD = false;
-bool Constant::IS_WEIGHTED = false;
-int Constant::COMMUNITY_POPULATION = 8;
+const bool Constant::MODEL = false; // true : LT ; false : IC
--- Constant.h
+++ Constant.h
@@ -1,18 +1,18 @@
 #pragma once
-class Constant
-{
+
+class Constant {
 public:
-	Constant();
-	~Constant();
+    Constant();
 
-	static int K;
-	static bool IS_WEIGHTED;
-	static int COMMUNITY_POPULATION;
-	static const double PERCENTAGE_THRESHOLD;
-	static const double EPSILON;
-	static const double DELTA;
-	static const int NUM_THREAD = 8;
-	static bool IS_BOUNDED_THRESHOLD;
-	static const bool MODEL = false; // true : LT ; false : IC
+    ~Constant();
+
+    static int K;
+    static bool IS_WEIGHTED;
+    static int COMMUNITY_POPULATION;
+    static const double PERCENTAGE_THRESHOLD;
+    static const double EPSILON;
+    static const double DELTA;
+    static const int NUM_THREAD;
+    static bool IS_BOUNDED_THRESHOLD;
+    static const bool MODEL; // true : LT ; false : IC
 };
-
